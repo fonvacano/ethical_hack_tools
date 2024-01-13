b23d9001bfbd04d36ff18ee51253f51d7e42049a
Shrek <alexandrproskr@gmail.com>
2024-01-13 15:47:44 +0400

    new simple tcp server

---
+++ simple_tcp_server/tcp_server.py
@@ -0,0 +1,21 @@
+from socket import *
+
+serverPort = 8000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+connection, addr = serverSocket.accept()
+connection.send(('Thank you for connecting to me ' + str(addr)).encode())
+print('Thank you for connecting to me ' + str(addr))
+message = connection.recv(1024).decode()
+print(message)
+command = ''
+while command != 'exit':
+    command = input('Enter command:')
+    connection.send(command.encode())
+    response = connection.recv(1024).decode()
+    print(response)
+connection.shutdown(SHUT_RDWR)
+connection.close()
+
